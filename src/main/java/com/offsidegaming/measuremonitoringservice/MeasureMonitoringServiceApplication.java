2663c7536d06139ed21d7e0befa43edd3f787c9a
aleksey.sugrobov <sugrobov.al@esphere.ru>
2023-11-23 13:37:43 +0100

    init project

---
+++ src/main/java/com/offsidegaming/measuremonitoringservice/MeasureMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.offsidegaming.measuremonitoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offsidegaming/measuremonitoringservice/MeasureMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offsidegaming.measuremonitoringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
